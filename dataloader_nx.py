f55f523336be7527abfdad1b2f10e80323bcf9fd
datvo06 <datvo06@gmail.com>
2021-07-23 21:51:44 +0700

    minor update

--- dataloader_nx.py
+++ dataloader_nx.py
@@ -283,3 +283,143 @@ class CodeflawsDGLDataset(DGLDataset):
 
     def __getitem__(self, i):
         return self.gs[i]
+
+
+class TestCodeflawsDGLDataset(DGLDataset):
+    def __init__(self, train_dataset, raw_dir=ConfigClass.raw_dir,
+                 save_dir=ConfigClass.preprocess_dir,
+                 label_mapping_path=ConfigClass.codeflaws_train_cfgidx_map_pkl,
+                 graph_opt=1):
+        self.mode = os.path.splitext(os.path.split(label_mapping_path)[-1])[0]
+        self.train_dataset = train_dataset
+        self.graph_save_path = os.path.join(
+            save_dir, f'dgl_nx_graphs_{graph_opt}_{self.mode}.bin')
+        self.graph_opt = graph_opt
+        self.nx_dataset = CodeflawsNxDataset(raw_dir, save_dir,
+                                             label_mapping_path, graph_opt)
+        self.vocab_dict = dict(tuple(line.split()) for line in open(
+            'preprocess/codeflaws_vocab.txt', 'r'))
+
+        super(CodeflawsDGLDataset, self).__init__(
+            name='codeflaws_dgl',
+            url=None,
+            raw_dir=raw_dir,
+            save_dir=save_dir,
+            force_reload=False,
+            verbose=False)
+
+        self.active_idxs = range(len(self.gs))
+        self.val_idxs = self.active_idxs
+
+    def val(self):
+        self.active_idxs = self.val_idxs
+
+    def has_cache(self):
+        return os.path.exists(self.graph_save_path)
+
+    def load(self):
+        self.gs = load_graphs(self.graph_save_path)[0]
+        self.meta_graph = self.train_dataset.meta_graph
+        self.cfg_content_dim = self.train_dataset.cfg_content_dim
+        self.ast_content_dim = self.train_dataset.ast_content_dim
+
+    def save(self):
+        os.makedirs(self.save_dir, exist_ok=True)
+        save_graphs(self.graph_save_path, self.gs)
+
+    def convert_from_nx_to_dgl(self, embedding_model, nx_g, lbl):
+        # Create a node mapping for cfg
+        n_cfgs = [n for n in nx_g.nodes() if nx_g.nodes[n]['graph'] == 'cfg']
+        cfg2id = dict([n, i] for i, n in enumerate(n_cfgs))
+        # Create a node mapping for ast
+        n_asts = [n for n in nx_g.nodes() if nx_g.nodes[n]['graph'] == 'ast']
+        ast2id = dict([n, i] for i, n in enumerate(n_asts))
+        # Create a node mapping for test
+        n_tests = [n for n in nx_g.nodes() if nx_g.nodes[n]['graph'] == 'test']
+        t2id = dict([n, i] for i, n in enumerate(n_tests))
+        map2id = {'cfg': cfg2id, 'ast': ast2id, 'test': t2id}
+
+        # Create dgl cfg node
+        cfg_labels = torch.tensor(
+            [ConfigClass.cfg_label_corpus.index(nx_g.nodes[n]['ntype'])
+             for n in n_cfgs], dtype=torch.long)
+        cfg_contents = torch.stack([
+            torch.from_numpy(embedding_model.get_sentence_vector(
+                nx_g.nodes[n]['text'].replace('\n', '')))
+            for n in n_cfgs], dim=0)
+        # Create dgl ast node
+        ast_labels = torch.tensor([
+            self.train_dataset.nx_dataset.ast_types.index(
+                nx_g.nodes[node]['ntype'])
+            for node in n_asts], dtype=torch.long
+        )
+        ast_contents = torch.stack([
+            torch.from_numpy(embedding_model.get_sentence_vector(
+                nx_g.nodes[n]['token'].replace('\n', '')))
+            for n in n_asts], dim=0)
+        # Create dgl test node
+        # No need, will be added automatically when we update edges
+
+        all_canon_etypes = {}
+        for k in self.meta_graph:
+            all_canon_etypes[k] = []
+        line2cfg = {}
+        for n in n_cfgs:
+            if nx_g.nodes[n]['end_line'] - nx_g.nodes[n]['start_line'] > 0:
+                continue
+            if nx_g.nodes[n]['start_line'] not in line2cfg:
+                line2cfg[nx_g.nodes[n]['start_line']] = [n]
+            else:
+                line2cfg[nx_g.nodes[n]['start_line']].append(n)
+        nx_g = augment_with_reverse_edge(
+            nx_g,
+            self.train_dataset.nx_dataset.ast_etypes,
+            self.train_dataset.nx_dataset.cfg_etypes)
+
+        for u, v, k, e in list(nx_g.edges(keys=True, data=True)):
+            map_u = map2id[nx_g.nodes[u]['graph']]
+            map_v = map2id[nx_g.nodes[v]['graph']]
+            all_canon_etypes[
+                (nx_g.nodes[u]['graph'], e['label'], nx_g.nodes[v]['graph'])
+            ].append([map_u[u], map_v[v]])
+
+        for k in all_canon_etypes:
+            if len(all_canon_etypes[k]) > 0:
+                type_es = torch.tensor(all_canon_etypes[k], dtype=torch.int32)
+                all_canon_etypes[k] = (type_es[:, 0], type_es[:, 1])
+            else:
+                all_canon_etypes[k] = (torch.tensor([], dtype=torch.int32),
+                                       torch.tensor([], dtype=torch.int32))
+        g = dgl.heterograph(all_canon_etypes)
+        g.nodes['cfg'].data['label'] = cfg_labels
+        g.nodes['cfg'].data['content'] = cfg_contents
+        g.nodes['ast'].data['label'] = ast_labels
+        g.nodes['ast'].data['content'] = ast_contents
+        g = dgl.add_self_loop(g, etype=('ast', 'a_self_loop', 'ast'))
+        g = dgl.add_self_loop(g, etype=('cfg', 'c_self_loop', 'cfg'))
+        tgts = torch.zeros(len(n_cfgs), dtype=torch.long)
+        for line in lbl:
+            while line not in line2cfg:
+                line -= 1   # Note: Matching to the closest (presumably parent)
+            for n in line2cfg[line]:
+                tgts[cfg2id[n]] = 1
+        g.nodes['cfg'].data['tgt'] = tgts
+        return g
+
+    def process(self):
+        self.meta_graph = self.construct_edge_metagraph()
+        self.gs = []
+        bar = tqdm.tqdm(enumerate(self.nx_dataset))
+        bar.set_description("Converting NX to DGL")
+        for i, (nx_g, lbl) in bar:
+            g = self.convert_from_nx_to_dgl(embedding_model, nx_g, lbl)
+            self.gs.append(g)
+            if i == 0:
+                self.cfg_content_dim = g.nodes['cfg'].data['content'].shape[-1]
+                self.ast_content_dim = g.nodes['ast'].data['content'].shape[-1]
+
+    def __len__(self):
+        return len(self.active_idxs)
+
+    def __getitem__(self, i):
+        return self.gs[i]
