e1679441d57ebd55616b068a23dc887da3aea03a
datvo06 <datvo06@gmail.com>
2021-07-26 10:13:24 +0700

    Minor fix

--- train_nx_astdiff.py
+++ train_nx_astdiff.py
@@ -35,39 +35,53 @@ class AverageMeter(object):
 
 
 class BinFullMeter(object):
-    def __init__(self):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
         self.reset()
 
     def reset(self):
-        self.tn = 0
-        self.tp = 0
-        self.fp = 0
-        self.fn = 0
+        self.tp = {}
+        self.tn = {}
+        self.fp = {}
+        self.fn = {}
+        for i in range(self.num_classes):
+            self.tn = 0
+            self.tp = 0
+            self.fp = 0
+            self.fn = 0
 
     def update(self, cal, labels):
-        self.tp += torch.sum((cal[labels == 1] == 1)).item()
-        self.tn += torch.sum((cal[labels == 0] == 0)).item()
-        self.fp += torch.sum((cal[labels == 0] == 1)).item()
-        self.fn += torch.sum((cal[labels == 1] == 0)).item()
+        for i in range(self.num_classes):
+            self.tp[i] += torch.sum((cal[labels == i] == i)).item()
+            self.tn[i] += torch.sum((cal[labels != i] != i)).item()
+            self.fp[i] += torch.sum((cal[labels != i] == i)).item()
+            self.fn[i] += torch.sum((cal[labels == i] != i)).item()
 
     def get(self):
-        tnr = 'unk'
-        tpr = 'unk'
-        prec = 'unk'
-        rec = 'unk'
-        aux_f1 = 'unk'
-        if self.tn + self.tp > 0:
-            tnr = self.tn/(self.tn + self.tp)
-        if (self.tp + self.tn) > 0:
-            tpr = self.tp/(self.tp + self.fn)
-        if (self.tp + self.fp) > 0:
-            prec = self.tp/(self.tp + self.fp)
-        if (self.tp + self.fn) > 0:
-            rec = self.tp/(self.tp + self.fn)
-        if prec != 'unk' and rec != 'unk':
-            aux_f1 = (prec + rec)/2
-        return {'tpr': tpr, 'tnr': tnr, 'prec': prec, 'rec': rec,
-                'aux_f1': aux_f1}
+        out_dict = {}
+        for i in range(self.num_classes):
+            tnr = 'unk'
+            tpr = 'unk'
+            prec = 'unk'
+            rec = 'unk'
+            aux_f1 = 'unk'
+            if self.tn[i] + self.tp[i] > 0:
+                tnr = self.tn[i]/(self.tn[i] + self.tp[i])
+            if (self.tp[i] + self.tn[i]) > 0:
+                tpr = self.tp[i]/(self.tp[i] + self.fn[i])
+            if (self.tp[i] + self.fp[i]) > 0:
+                prec = self.tp[i]/(self.tp[i] + self.fp[i])
+            if (self.tp[i] + self.fn[i]) > 0:
+                rec = self.tp[i]/(self.tp[i] + self.fn[i])
+            if prec != 'unk' and rec != 'unk':
+                aux_f1 = (prec + rec)/2
+        out_dict[i] = {'tpr': tpr, 'tnr': tnr, 'prec': prec, 'rec': rec,
+                       'aux_f1': aux_f1}
+        if all(out_dict[i]['aux_f1'] != 'unk' for i in range(self.num_classes)):
+            out_dict['aux_f1'] = sum(out_dict[i]['aux_f1']
+                                     for i in range(self.num_classes))/self.num_classes
+        else:
+            out_dict['aux_f1'] = 'unk'
 
 
 def as_minutes(s):
@@ -97,7 +111,7 @@ def train(model, dataloader, n_epochs):
     top_5_meter = AverageMeter()
     top_10_meter = AverageMeter()
 
-    f1_meter = BinFullMeter()
+    f1_meter = BinFullMeter(3)
     best_f1 = 0.0
     # model.load_state_dict(torch.load("trained/model_27.pth"))
     # eval(model, dataloader)
@@ -217,7 +231,7 @@ def eval(model, dataloader, epoch):
     # mean_acc = AverageMeter()
     mean_ast_loss = AverageMeter()
     mean_ast_acc = AverageMeter()
-    f1_meter = BinFullMeter()
+    f1_meter = BinFullMeter(3)
     top_1_meter = AverageMeter()
     top_2_meter = AverageMeter()
     top_5_meter = AverageMeter()
