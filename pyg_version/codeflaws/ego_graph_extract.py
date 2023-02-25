e7f68f61ed7cb0159d75a85a1f7922b2bdf37651
datvo06 <datvo06@gmail.com>
2023-02-25 11:39:19 +1100

    Adding graph numerization module

---
+++ pyg_version/codeflaws/ego_graph_extract.py
@@ -0,0 +1,147 @@
+from codeflaws.dataloader_cfl import CodeflawsCFLNxStatementDataset, \
+    CodeflawsCFLStatementGraphMetadata
+from pyg_version.codeflaws.dataloader_cfl_pyg import CodeflawsCFLPyGStatementDataset, CodeflawsCFLStatementGraphMetadata
+from pyg_version.model import MPNNModel_A_T_L
+from pyg_version.explainer.explainer import Explainer, InflExtractor
+from pyg_version.explainer.common import entropy_loss_mask, size_loss
+from graph_algos.nx_shortcuts import where_node, where_edge
+# Todo: use edge weight and feature weight balanced
+import torch
+import networkx as nx
+import os
+import argparse
+from pyg_version.codeflaws.explain_nx_a_nc_cfl_pyg import from_data_to_nx
+from typing import Tuple, Dict
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Todo: sample ego graph from the whole graph, the ratio of positive/negative
+# nodes is determined by a ratio
+# If a node belong to both positive and negative, it is considered as unknown
+
+
+class TopKTripletStatementIterator(object):
+
+    def __init__(self, model, dataset: CodeflawsCFLPyGStatementDataset, k,
+                 device):
+        self.model = model
+        self.dataset = dataset
+        self.k = k
+        self.len_data = self.calc_len()
+        self.device = device
+        self.model = self.model
+
+    def calc_len(self):
+        # loop through the dataset and check how many statements
+        # are in the data
+        # return the number of statements
+        tot = 0
+        for data, stmt_nodes in self.dataset:
+            tot += min(len(stmt_nodes), self.k)
+        return tot
+
+    def __iter__(self):
+        self.model.eval()
+        with torch.no_grad():
+            for data, stmt_nodes in self.dataset:
+                data = data.to(device)
+                output = self.model(data.xs, data.ess)[0]
+                output = output[stmt_nodes, 1].cpu()
+                k = min(len(stmt_nodes), self.k)
+                totlen = len(stmt_nodes)
+                k_pos = list(range(k - max(k-totlen//2, 0)))
+                k_uncertain = list(range(k - k_pos, k + k_pos))
+                k_neg = list(range(k + k_pos, k))
+                yield (data, stmt_nodes.to(device)), output, k_pos, k_uncertain, k_neg
+
+    def __len__(self):
+        return self.len_data
+
+
+class EgoGraphExtractor(object):
+
+    def __init__(self, model, dataset: CodeflawsCFLPyGStatementDataset, k, hops,
+                 meta_data, device): 
+        self.triplet_iter = TopKTripletStatementIterator(model, dataset, k, device)
+        self.meta_data = meta_data
+        self.hops = hops
+
+    def extract(self) -> nx.MultiDiGraph:
+        for (data, stmt_nodes), output, k_pos, k_uncertain, k_neg in self.triplet_iter:
+            # 1. convert data to nx
+            graph = from_data_to_nx(data, None, self.meta_data)
+            # 2. extract the ego graph from the nx
+            # 2.1 extract the top k positive nodes
+            pos_ego_graphs = []
+            for pos_node in stmt_nodes[k_pos]:
+                ego_graph = nx.ego_graph(graph, pos_node, self.hops, undirected=True).copy()
+                ego_graph.nodes[pos_node]['is_target'] = 1
+                ego_graph.nodes[pos_node]['color'] = 'red'
+                ego_graph.nodes[pos_node]['style'] = 'filled'
+                pos_ego_graphs.append(ego_graph)
+            # 2.2 extract the top k negative nodes
+            neg_ego_graphs = []
+            for neg_node in stmt_nodes[k_neg]:
+                ego_graph = nx.ego_graph(graph, neg_node, self.hops, undirected=True).copy()
+                ego_graph.nodes[neg_node]['is_target'] = 1
+                ego_graph.nodes[neg_node]['color'] = 'red'
+                ego_graph.nodes[neg_node]['style'] = 'filled'
+                neg_ego_graphs.append(ego_graph)
+            # 2.3 extract the top k uncertain nodes
+            uncertain_ego_graphs = []
+            for uncertain_node in stmt_nodes[k_uncertain]:
+                ego_graph = nx.ego_graph(graph, uncertain_node, self.hops, undirected=True).copy()
+                ego_graph.nodes[uncertain_node]['is_target'] = 1
+                ego_graph.nodes[uncertain_node]['color'] = 'red'
+                ego_graph.nodes[uncertain_node]['style'] = 'filled'
+                uncertain_ego_graphs.append(ego_graph)
+            # 3. yield the ego graph
+            yield pos_ego_graphs, neg_ego_graphs, uncertain_ego_graphs
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--save_path",
+                        type=str,
+                        default="ego_pyg_codeflaws_pyc_cfl_stmt_level")
+    parser.add_argument("--k", type=int, default=10)
+    parser.add_argument("--hops", type=int, default=5)
+    # parser.add_argument("--loss_func", type=str,
+    #                     default="total_loss_size_stmt_entropy")
+    parser.add_argument("--device", type=str, default='cuda')
+    return parser.parse_args()
+
+
+
+def main():
+    t2id = {'ast': 0, 'test': 1}
+    args = get_args()
+    nx_dataset = CodeflawsCFLNxStatementDataset()
+    meta_data = CodeflawsCFLStatementGraphMetadata(nx_dataset)
+
+    model = MPNNModel_A_T_L(dim_h=64,
+                            netypes=len(meta_data.meta_graph),
+                            t_srcs=[t2id[e[0]] for e in meta_data.meta_graph],
+                            t_tgts=[t2id[e[2]] for e in meta_data.meta_graph],
+                            n_al=len(meta_data.t_asts),
+                            n_layers=5,
+                            n_classes=2).to(device)
+
+    model.load_state_dict(torch.load(args.model_path, map_location=device))
+    model.eval()
+    save_path = args.save_path
+    os.makedirs(save_path, exist_ok=True)
+    ego_graph_extractor = EgoGraphExtractor(model, nx_dataset, args.k, args.hops,
+                                            meta_data, device)
+    for i, (pos_ego_graphs, neg_ego_graphs, uncertain_ego_graphs) in enumerate(ego_graph_extractor.extract()):
+        for j, ego_graph in enumerate(pos_ego_graphs):
+            nx.write_gpickle(ego_graph, os.path.join(save_path, f"pos_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'pos_{i}_{j}.dot'))
+        for j, ego_graph in enumerate(neg_ego_graphs):
+            nx.write_gpickle(ego_graph, os.path.join(save_path, f"pos_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'neg_{i}_{j}.dot'))
+        for j, ego_graph in enumerate(uncertain_ego_graphs):
+            nx.write_gpickle(ego_graph, os.path.join(save_path, f"pos_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'uncertain_{i}_{j}.dot'))
