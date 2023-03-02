93b25c52371b0b99d4782a982735932c30a1ba79
datvo06 <datvo06@gmail.com>
2023-03-02 19:35:44 +1100

    minor

--- pyg_version/codeflaws/ego_graph_extract.py
+++ pyg_version/codeflaws/ego_graph_extract.py
@@ -1,5 +1,6 @@
 from codeflaws.dataloader_cfl import CodeflawsCFLNxStatementDataset
-from pyg_version.dataloader_cfl_pyg import PyGStatementDataset, AstGraphMetadata
+from pyg_version.dataloader_cfl_pyg import (
+    PyGStatementDataset, AstGraphMetadata, nx_to_pyg_stmt)
 from pyg_version.model import MPNNModel_A_T_L
 from pyg_version.explainer.explainer import Explainer, InflExtractor
 from pyg_version.explainer.common import entropy_loss_mask, size_loss
@@ -50,8 +51,10 @@ class TopKTripletStatementIterator(object):
                 totlen = len(stmt_nodes)
                 topk = output.topk(totlen, dim=0)[1]
                 k_pos = list(range(k - max(k-totlen//2, 0)))
-                k_uncertain = list(range(k - max(k-totlen//2, 0), min(k + max(k-totlen//2, 0), 0)))
-                k_neg = list(range(totlen - max((k - max(k-totlen//2, 0)), 0), totlen))
+                k_uncertain = list(
+                    range(k - max(k-totlen//2, 0), min(k + max(k-totlen//2, 0), 0)))
+                k_neg = list(
+                    range(totlen - max((k - max(k-totlen//2, 0)), 0), totlen))
                 yield (data, stmt_nodes.to(device)), output, topk[k_pos], topk[k_uncertain], topk[k_neg]
 
     def __len__(self):
@@ -62,7 +65,8 @@ class EgoGraphExtractor(object):
 
     def __init__(self, model, dataset: PyGStatementDataset, k, hops,
                  meta_data, device):
-        self.triplet_iter = TopKTripletStatementIterator(model, dataset, k, device)
+        self.triplet_iter = TopKTripletStatementIterator(
+            model, dataset, k, device)
         self.meta_data = meta_data
         self.hops = hops
 
@@ -76,7 +80,8 @@ class EgoGraphExtractor(object):
             # 2.1 extract the top k positive nodes
             pos_ego_graphs = []
             for pos_node in stmt_nodes[k_pos]:
-                ego_graph = nx.ego_graph(graph, f"ast_{pos_node}", self.hops, undirected=True).copy()
+                ego_graph = nx.ego_graph(
+                    graph, f"ast_{pos_node}", self.hops, undirected=True).copy()
                 ego_graph.nodes[f"ast_{pos_node}"]['is_target'] = 1
                 ego_graph.nodes[f"ast_{pos_node}"]['color'] = 'red'
                 ego_graph.nodes[f"ast_{pos_node}"]['style'] = 'filled'
@@ -84,7 +89,8 @@ class EgoGraphExtractor(object):
             # 2.2 extract the top k negative nodes
             neg_ego_graphs = []
             for neg_node in stmt_nodes[k_neg]:
-                ego_graph = nx.ego_graph(graph, f"ast_{neg_node}", self.hops, undirected=True).copy()
+                ego_graph = nx.ego_graph(
+                    graph, f"ast_{neg_node}", self.hops, undirected=True).copy()
                 ego_graph.nodes[f"ast_{neg_node}"]['is_target'] = 1
                 ego_graph.nodes[f"ast_{neg_node}"]['color'] = 'red'
                 ego_graph.nodes[f"ast_{neg_node}"]['style'] = 'filled'
@@ -92,14 +98,14 @@ class EgoGraphExtractor(object):
             # 2.3 extract the top k uncertain nodes
             uncertain_ego_graphs = []
             for uncertain_node in stmt_nodes[k_uncertain]:
-                ego_graph = nx.ego_graph(graph, f"ast_{uncertain_node}", self.hops, undirected=True).copy()
+                ego_graph = nx.ego_graph(
+                    graph, f"ast_{uncertain_node}", self.hops, undirected=True).copy()
                 ego_graph.nodes[f"ast_{uncertain_node}"]['is_target'] = 1
                 ego_graph.nodes[f"ast_{uncertain_node}"]['color'] = 'red'
                 ego_graph.nodes[f"ast_{uncertain_node}"]['style'] = 'filled'
                 uncertain_ego_graphs.append(ego_graph)
             # 3. yield the ego graph
             yield pos_ego_graphs, neg_ego_graphs, uncertain_ego_graphs
-
 
 
 def get_args():
@@ -116,12 +122,12 @@ def get_args():
     return parser.parse_args()
 
 
-
 def main():
     t2id = {'ast': 0, 'test': 1}
     args = get_args()
     print("Loading data...")
     nx_dataset = CodeflawsCFLNxStatementDataset()
+    meta_data = AstGraphMetadata(nx_dataset)
     #### TEST ####
     os.makedirs("tmp", exist_ok=True)
     for i, (nx_g, stmt_nodes) in enumerate(nx_dataset):
@@ -131,15 +137,17 @@ def main():
             nx_g.nodes[node].pop("name", None)
         for edge in nx_g.edges:
             nx_g.edges[edge].pop("name", None)
+        metamorphed_nx = from_data_to_nx(nx_to_pyg_stmt(meta_data, nx_g, None, stmt_nodes)[0], None, meta_data)
+        nx.write_gpickle(metamorphed_nx, f"tmp/{i}_mm.gpickle")
         nx.drawing.nx_pydot.write_dot(nx_g, f"tmp/{i}.dot")
+        nx.drawing.nx_pydot.write_dot(metamorphed_nx, f"tmp/{i}_mm.dot")
     #### END TEST ####
-    meta_data = AstGraphMetadata(nx_dataset)
     pyg_dataset = PyGStatementDataset(
-            dataloader=nx_dataset,
-            meta_data=meta_data,
-            ast_enc=None,
-            save_dir="preprocessed/codeflaws/",
-            name='train_pyg_cfl_stmt')
+        dataloader=nx_dataset,
+        meta_data=meta_data,
+        ast_enc=None,
+        save_dir="preprocessed/codeflaws/",
+        name='train_pyg_cfl_stmt')
     print("Loading model...")
     model = MPNNModel_A_T_L(dim_h=64,
                             netypes=len(meta_data.meta_graph),
@@ -158,14 +166,20 @@ def main():
                                             meta_data, device)
     for i, (pos_ego_graphs, neg_ego_graphs, uncertain_ego_graphs) in enumerate(ego_graph_extractor.extract()):
         for j, ego_graph in enumerate(pos_ego_graphs):
-            nx.write_gpickle(ego_graph, os.path.join(save_path, f"pos_{i}_{j}.gpickle"))
-            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'pos_{i}_{j}.dot'))
+            nx.write_gpickle(ego_graph, os.path.join(
+                save_path, f"pos_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(
+                ego_graph, os.path.join(save_path, f'pos_{i}_{j}.dot'))
         for j, ego_graph in enumerate(neg_ego_graphs):
-            nx.write_gpickle(ego_graph, os.path.join(save_path, f"neg_{i}_{j}.gpickle"))
-            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'neg_{i}_{j}.dot'))
+            nx.write_gpickle(ego_graph, os.path.join(
+                save_path, f"neg_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(
+                ego_graph, os.path.join(save_path, f'neg_{i}_{j}.dot'))
         for j, ego_graph in enumerate(uncertain_ego_graphs):
-            nx.write_gpickle(ego_graph, os.path.join(save_path, f"uncertain_{i}_{j}.gpickle"))
-            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(save_path, f'uncertain_{i}_{j}.dot'))
+            nx.write_gpickle(ego_graph, os.path.join(
+                save_path, f"uncertain_{i}_{j}.gpickle"))
+            nx.drawing.nx_pydot.write_dot(ego_graph, os.path.join(
+                save_path, f'uncertain_{i}_{j}.dot'))
 
 
 if __name__ == "__main__":
