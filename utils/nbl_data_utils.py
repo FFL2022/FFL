6eddb3b7a778036fdee3b13d231d896fceded35a
datvo06 <datvo06@gmail.com>
2023-09-01 20:36:23 +1000

    Remove redundant file

--- utils/nbl_data_utils.py
+++
@@ -1,9 +0,0 @@
-def make_nbl_dict(key, test_verdict):
-    problem_id, uid, program_id = key.split("-")
-    nbl = {}
-    nbl['problem_id'] = problem_id
-    nbl['uid'] = uid
-    nbl['program_id'] = program_id
-    nbl['test_verdict'] = test_verdict[problem_id][int(program_id)]
-    return nbl
-
