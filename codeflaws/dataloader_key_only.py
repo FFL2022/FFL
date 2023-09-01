37ffba3b9c17ae82c341db8333098f4647c40ef8
datvo06 <datvo06@gmail.com>
2023-09-01 17:16:32 +1000

    Minor syntax fix

--- codeflaws/dataloader_key_only.py
+++ codeflaws/dataloader_key_only.py
@@ -27,8 +27,8 @@ class CodeflawsNxDataset(AstNxDataset):
                 all_entries=all_codeflaws_keys,
                 process_func=lambda k: get_cfg_ast_cov(k)[-1],
                 save_dir=save_dir, name='key_only',
-                special_attrs=[
+                special_attrs={
                     'ast_lb_d': lambda nx_g: nodes_where(nx_g, graph='ast', status=1),
                     'ast_lb_i': lambda nx_g: nodes_where(nx_g, graph='ast', status=2),
-                    'cfg_lb': lambda nx_g: nodes_where(nx_g, graph='cfg', status=1)], post_process_func=del_all_status)
+                    'cfg_lb': lambda nx_g: nodes_where(nx_g, graph='cfg', status=1)}, post_process_func=del_all_status)
         self.cfg_etypes = ['parent_child', 'next', 'ref', 'func_call']
